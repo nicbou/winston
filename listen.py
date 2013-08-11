918ba623977a0bc0146143e4e1a8cb2ba8066204
Nicolas Bouliane <contact@nicolasbouliane.com>
2013-08-10 22:18:46 -0400

    Updated the grammar, implemented active listening

--- listen.py
+++ listen.py
@@ -1,92 +1,93 @@
 #!/usr/bin/env python
 
-import sys,os
-from os import system
-from datetime import datetime
-from time import strftime
+# Copyright (c) 2008 Carnegie Mellon University.
+#
+# You may modify and redistribute this file under the same terms as
+# the CMU Sphinx system.  See
+# http://cmusphinx.sourceforge.net/html/LICENSE for more information.
 
-import wave
-import pyaudio
+import pygtk
+pygtk.require('2.0')
+import gtk
 
-def decodeSpeech(hmmd,lmdir,dictp,jsgf,wavfile):
-    """
-    Decodes a speech file
-    """
-    try:
-      from pocketsphinx import Decoder
-    except:
-      from pocketsphinx import Decoder
+import gobject
+import pygst
+pygst.require('0.10')
+gobject.threads_init()
+import gst
 
-    import sphinxbase
-    speechRec = Decoder(hmm=hmmd, lm=lmdir, dict=dictp, jsgf=jsgf, rate=16000)
-    wavFile = file(wavfile,'rb')
-    speechRec.decode_raw(wavFile)
-    result = speechRec.get_hyp()
+class Listener(object):
+    """GStreamer/PocketSphinx Demo Application"""
+    def __init__(self):
+        """Initialize a DemoApp object"""
+        self.init_gui()
+        self.init_gst()
 
-    return result[0]
+    def init_gui(self):
+        """Initialize the GUI components"""
+        self.window = gtk.Window()
+        self.window.connect("delete-event", gtk.main_quit)
+        self.window.set_default_size(400,200)
+        self.window.set_border_width(10)
+        vbox = gtk.VBox()
+        self.textbuf = gtk.TextBuffer()
+        self.text = gtk.TextView(self.textbuf)
+        self.text.set_wrap_mode(gtk.WRAP_WORD)
+        vbox.pack_start(self.text)
+        self.button = gtk.ToggleButton("Speak")
+        vbox.pack_start(self.button, False, False, 5)
+        self.window.add(vbox)
+        self.window.show_all()
 
-def record():
+    def init_gst(self):
+        """Initialize the speech components"""
+        self.pipeline = gst.parse_launch('gconfaudiosrc ! audioconvert ! audioresample '
+                                         + '! vader name=vad auto_threshold=true '
+                                         + '! pocketsphinx name=asr ! fakesink')
+        asr = self.pipeline.get_by_name('asr')
 
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 2
-    RATE = 44100
-    RECORD_SECONDS = 3
-    WAVE_OUTPUT_FILENAME = "output.wav"
+        # Bind the result
+        asr.connect('partial_result', self.asr_partial_result)
+        asr.connect('result', self.asr_result)
 
-    p = pyaudio.PyAudio()
+        # Load the grammar file (generated from the jsgf file)
+        asr.set_property("fsg", "grammar.fsg")
+        asr.set_property('configured', True)
 
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
+        bus = self.pipeline.get_bus()
+        bus.add_signal_watch()
+        bus.connect('message::application', self.application_message)
 
-    print("* recording")
+        self.pipeline.set_state(gst.STATE_PLAYING)
 
-    frames = []
+    def asr_partial_result(self, asr, text, uttid):
+        """Forward partial result signals on the bus to the main thread."""
+        struct = gst.Structure('partial_result')
+        struct.set_value('hyp', text)
+        struct.set_value('uttid', uttid)
+        asr.post_message(gst.message_new_application(asr, struct))
 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
+    def asr_result(self, asr, text, uttid):
+        """Forward partial result signals on the bus to the main thread."""
+        struct = gst.Structure('result')
+        struct.set_value('hyp', text)
+        struct.set_value('uttid', uttid)
+        asr.post_message(gst.message_new_application(asr, struct))
 
-    print("* done recording")
+    def application_message(self, bus, msg):
+        """Receive application messages from the bus."""
+        msgtype = msg.structure.get_name()
+        self.partial_result(msg.structure['hyp'], msg.structure['uttid'])
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+    def partial_result(self, hyp, uttid):
+        """Delete any previous selection, insert text and select it."""
+        # All this stuff appears as one single action
+        print("Partial result: " + hyp)
 
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
+    def result(self, hyp, uttid):
+        """Delete any previous selection, insert text and select it."""
+        # All this stuff appears as one single action
+        print("Result: " + hyp)
 
-if __name__ == "__main__":
-
-    hmdir = "/users/nicolas/Dropbox/ReposiTories/SpeechRec/model/hmm/wsj1"
-    lmd = "/usr/local/Cellar/cmu-pocketsphinx/0.8/share/pocketsphinx/model/lm/en_US/wsj0vp.5000.DMP"
-    dictd = "/usr/local/Cellar/cmu-pocketsphinx/0.8/share/pocketsphinx/model/lm/en_US/cmu07a.dic"
-    jsgf = "/users/nicolas/Dropbox/ReposiTories/SpeechRec/jsgf.txt"
-    wavfile = "/users/nicolas/Dropbox/ReposiTories/SpeechRec/output.wav"
-
-    record()
-
-    phrase = decodeSpeech(hmdir,lmd,dictd,jsgf,wavfile)
-
-    answer = 'I am sorry. I did not understand that'
-    if phrase == 'jenkins what time is it':
-      answer = 'I do not know'
-    elif phrase == 'jenkins what day is it' or phrase == 'jenkins what is the date':
-      answer = 'It is ' + strftime("%B %d")
-    elif phrase == 'jenkins say hello':
-      answer = 'Hello'
-
-
-    system('say %s' % answer)
-
-    print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-
-    print phrase
-
-    print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+app = Listener()
+gtk.main()
