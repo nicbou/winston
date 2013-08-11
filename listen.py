4497bff52d942c154e16f51d2123e7b8c403b3ed
Nicolas Bouliane <contact@nicolasbouliane.com>
2013-08-10 22:45:35 -0400

    Simplified the Listener, turned it into a reusable class

--- listen.py
+++ listen.py
@@ -1,15 +1,3 @@
-#!/usr/bin/env python
-
-# Copyright (c) 2008 Carnegie Mellon University.
-#
-# You may modify and redistribute this file under the same terms as
-# the CMU Sphinx system.  See
-# http://cmusphinx.sourceforge.net/html/LICENSE for more information.
-
-import pygtk
-pygtk.require('2.0')
-import gtk
-
 import gobject
 import pygst
 pygst.require('0.10')
@@ -17,37 +5,30 @@ gobject.threads_init()
 import gst
 
 class Listener(object):
-    """GStreamer/PocketSphinx Demo Application"""
-    def __init__(self):
-        """Initialize a DemoApp object"""
-        self.init_gui()
-        self.init_gst()
+    """
+    Listens, understands and processes speeches using the
+    python-gstreamer plugin.
+    """
+    def __init__(self, start=True):
+        """
+        Initialize the listener
+        """
+        # Init gstreamer
+        self.init_gstreamer()
 
-    def init_gui(self):
-        """Initialize the GUI components"""
-        self.window = gtk.Window()
-        self.window.connect("delete-event", gtk.main_quit)
-        self.window.set_default_size(400,200)
-        self.window.set_border_width(10)
-        vbox = gtk.VBox()
-        self.textbuf = gtk.TextBuffer()
-        self.text = gtk.TextView(self.textbuf)
-        self.text.set_wrap_mode(gtk.WRAP_WORD)
-        vbox.pack_start(self.text)
-        self.button = gtk.ToggleButton("Speak")
-        vbox.pack_start(self.button, False, False, 5)
-        self.window.add(vbox)
-        self.window.show_all()
+        # Start listening
+        if start:
+            self.start()
 
-    def init_gst(self):
-        """Initialize the speech components"""
+    def init_gstreamer(self):
+        # Get the pipeline
         self.pipeline = gst.parse_launch('gconfaudiosrc ! audioconvert ! audioresample '
                                          + '! vader name=vad auto_threshold=true '
                                          + '! pocketsphinx name=asr ! fakesink')
         asr = self.pipeline.get_by_name('asr')
 
-        # Bind the result
-        asr.connect('partial_result', self.asr_partial_result)
+        # Bind the pipeline results
+        asr.connect('partial_result', self.asr_result)
         asr.connect('result', self.asr_result)
 
         # Load the grammar file (generated from the jsgf file)
@@ -56,38 +37,31 @@ class Listener(object):
 
         bus = self.pipeline.get_bus()
         bus.add_signal_watch()
-        bus.connect('message::application', self.application_message)
+        
+        self.pipeline.set_state(gst.STATE_PAUSED)
 
+    def asr_result(self, asr, parsed_text, utterance_id):
+        """
+        Receives a result from the pipeline, and forwards the parsed
+        text to process_result, which is intended to be overridden.
+        """
+        self.process_result(parsed_text)
+
+    def start(self):
         self.pipeline.set_state(gst.STATE_PLAYING)
 
-    def asr_partial_result(self, asr, text, uttid):
-        """Forward partial result signals on the bus to the main thread."""
-        struct = gst.Structure('partial_result')
-        struct.set_value('hyp', text)
-        struct.set_value('uttid', uttid)
-        asr.post_message(gst.message_new_application(asr, struct))
+    def pause(self):
+        self.pipeline.set_state(gst.STATE_PAUSED)
 
-    def asr_result(self, asr, text, uttid):
-        """Forward partial result signals on the bus to the main thread."""
-        struct = gst.Structure('result')
-        struct.set_value('hyp', text)
-        struct.set_value('uttid', uttid)
-        asr.post_message(gst.message_new_application(asr, struct))
+    def process_result(self, parsed_text):
+        """
+        Does something with the recognized sentence. Override this function
+        to define custom functionality.
+        """
+        print(parsed_text)
 
-    def application_message(self, bus, msg):
-        """Receive application messages from the bus."""
-        msgtype = msg.structure.get_name()
-        self.partial_result(msg.structure['hyp'], msg.structure['uttid'])
+# Get a listener
+listener = Listener()
 
-    def partial_result(self, hyp, uttid):
-        """Delete any previous selection, insert text and select it."""
-        # All this stuff appears as one single action
-        print("Partial result: " + hyp)
-
-    def result(self, hyp, uttid):
-        """Delete any previous selection, insert text and select it."""
-        # All this stuff appears as one single action
-        print("Result: " + hyp)
-
-app = Listener()
-gtk.main()
+# And wait...
+raw_input()
