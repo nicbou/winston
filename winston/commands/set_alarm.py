0d321662705b892dadb5c17467a248c51a2042f0
Nicolas Bouliane <contact@nicolasbouliane.com>
2013-08-25 11:31:16 -0400

    Finished the alarm command

--- winston/commands/set_alarm.py
+++ winston/commands/set_alarm.py
@@ -1,6 +1,9 @@
-from utils.texttospeech import text_to_speech
 from commands import Command
-import re
+from datetime import time
+from os import system
+from random import choice
+from utils.texttospeech import __file__ as tts_path
+from utils.texttospeech import text_to_speech
 
 one_to_ten = ('one','two','three','four','five','six','seven','eight','nine','ten',)
 eleven_to_nineteen = ('eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen',)
@@ -19,10 +22,20 @@ class AbsoluteAlarmCommand(Command):
 
     def set_alarm(self, when):
         # Remove the clutter
-        when.replace('tomorrow', '')
+        when = when.replace('tomorrow', '')
 
         #Parse the number
+        time_value = text_to_time(when)
+        time_string = time_value.strftime("%H:%M")
+        command = 'echo python {script} "{message}" | at -m {time}AM'
 
+        messages = [
+            "Sir, you asked me to set an alarm at this precise moment.",
+            "It is time sir. You asked me to set an alarm at this precise moment.",
+            "Hello sir. You asked me to wake you up at this time.",
+        ]
+
+        system(command.format(script=tts_path, time=time_string, message=choice(messages)))
         text_to_speech("Setting an alarm in " + when )
 
 
@@ -38,9 +51,6 @@ class RelativeAlarmCommand(Command):
             '|'.join(twenty_to_ninety),
             '|'.join(one_to_ten),
         )
-        print(self.subjects)
-
-
         super(RelativeAlarmCommand, self).__init__(actions=self.actions, subjects=self.subjects, callback=self.set_alarm, name=name)
 
     def set_alarm(self, when):
@@ -49,48 +59,70 @@ class RelativeAlarmCommand(Command):
         when.replace(' from now', '')
 
         #Parse the number
+        if "hours" in when:
+            command = 'echo python {script} "{message}" | at now + {time} hours'
+        else:
+            command = 'echo python {script} "{message}" | at now + {time} minutes'
 
+        messages = [
+            "Sir, {time} ago, you asked me to set an alarm at this precise moment.".format(time=when),
+            "It is time sir. You asked me to set an alarm at this precise moment {time} ago.".format(time=when),
+        ]
+
+        system(command.format(script=tts_path, time=text_to_number(when), message=choice(messages)))
         text_to_speech("Setting an alarm in " + when )
+
 
 def text_to_number(text):
     """
-    Parses a number between 1 and 99 from a given string (like "twenty four")
+    Parses a number between 1 and 99 from a given string (like "twenty four").
+    This function expects space-separated words, not grammatically correct
+    numbers.
     """
+    total_value = 0
     while text:
         text = text.strip()
 
         # Get the first word, keep the rest of the string
-        text = string.split(' ',1)
-        text = text[1]  # The rest of the string
-        first_word = text[0]
+        split = text.split(' ',1)
+        if len(split) > 1:
+            text = split[1]  # The rest of the string
+        else:
+            text = ''
+        current_word = split[0]  # The first word
         
         # Get the value of the word
+        try:
+            total_value += one_to_ten.index(current_word) + 1
+        except:
+            pass
 
-    if not numwords:
-        units = [
-            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
-            "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-            "sixteen", "seventeen", "eighteen", "nineteen",
-        ]
+        try:
+            total_value += eleven_to_nineteen.index(current_word) + 11
+        except:
+            pass
 
-        tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+        try:
+            total_value += (twenty_to_ninety.index(current_word) * 10) + 20
+        except:
+            pass
 
-        scales = ["hundred", "thousand", "million", "billion", "trillion"]
+    return total_value
 
-        numwords["and"] = (1, 0)
-        for idx, word in enumerate(units):    numwords[word] = (1, idx)
-        for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
-        for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
 
-    current = result = 0
-    for word in textnum.split():
-        if word not in numwords:
-          raise Exception("Illegal word: " + word)
+def text_to_time(text):
+    """
+    Parses a time from a given string (like "six thirty" or "four forty four").
+    This function expects space-separated words, not grammatically correct
+    numbers.
+    """
+     # Get the first word, keep the rest of the string
+    split = text.split(' ',1)
 
-        scale, increment = numwords[word]
-        current = current * scale + increment
-        if scale > 100:
-            result += current
-            current = 0
+    hour = text_to_number(split[0])  # The first word is the hour
+    if len(split) > 1:
+        minutes = text_to_number(split[1])  # The rest of the string
+    else:
+        minutes = 0
 
-    return result + current
+    return time(hour, minutes)
